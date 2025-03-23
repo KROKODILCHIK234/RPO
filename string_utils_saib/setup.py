57fe8bbebbaedd3607e13d8f0f0470adb1974238
MALISMF <you@example.com>
2025-03-23 21:45:26 +0800

    setup update

--- string_utils_saib/setup.py
+++ string_utils_saib/setup.py
@@ -1,14 +1,25 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="string_utils",
-    version="0.1",
-    license='MIT',
+    name="my_string_utils",
+    version="0.2",  
+    license="MIT",
     packages=find_packages(),
     install_requires=[],
     python_requires=">=3.6",
     description="simple package for string utilities",
+    long_description="""
+    # String Utils
+
+    A simple package for string utilities.
+
+    ## GitHub Repository
+
+    You can find the source code on [GitHub](https://github.com/KROKODILCHIK234/RPO/tree/main/string_utils_saib).
+    """,
+    long_description_content_type="text/markdown",
     author="matvey",
-    author_email="matveyfox.mf@gmail.com"
+    author_email="matveyfox.mf@gmail.com",
+    url="https://github.com/KROKODILCHIK234/RPO/tree/main/string_utils_saib",  
 
 )
